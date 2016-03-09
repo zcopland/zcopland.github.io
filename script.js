d764df0bcc453d6f374c1cf6a6fa5fe46ace553d
Zach <zcopland16@gmail.com>
2016-03-09 08:30:12 -0500

    Added code for notification

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// create the notification popup
+var popup = q.create("<div>").appendTo(document.body).addClass("popup").setStyle("border-radius", "5px");
 
+// create the notification API
+var notify = function(message, delay, callback) {
+  popup.setHtml(message);
+  popup.fadeIn().once("animationEnd", function() {
+    window.setTimeout(function() {
+       popup.fadeOut().once("animationEnd", function() {
+         callback && callback.call();
+       });
+    }, delay);
+  });
+};
+
+// DEMO
+notify("This is ...", 1000, function() {
+  notify("... a qx.website notification demo.", 2000);
+});
