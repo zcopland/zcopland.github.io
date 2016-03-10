2fdc50dbef0d99e062d5c9ecf5d727bd622fe171
Zach Copland <zcopland16@gmail.com>
2016-03-10 08:40:43 -0500

    Attempted form verification

--- signupScript.js
+++ signupScript.js
@@ -1,1 +1,27 @@
+window.addEventListener("load", function() {
+	/* var submitBtn = document.getElementById("submitBtn");
+	submitBtn.addEventListener("click", function () {
+		//declaring variables
+		var firstName = document.getElementById("firstName").value;
+		var lastName = document.getElementById("lastName").value;
+		var emailAddress = document.getElementById("emailAddress").value;
+		console.log (firstName);
 
+		//checking to see if form is filled out
+		if (firstName.length > 2 && lastName.length > 3 && emailAddress.length >= 7) {
+			//now checking to see if email address is valid
+			/*function validateEmail(emailAddress) {
+	    		var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	    		return re.test(emailAddress);
+			}
+			if (re.test) {
+				alert(true);
+			} */ /*
+			alert("Your form has been submitted. Thank you.");
+		} 
+		//form has not been filled out
+		else {
+			alert("Please fill out the required fields.");
+		}
+	}) */
+});
