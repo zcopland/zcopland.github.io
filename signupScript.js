50ca4cf69be9c3ee30d84ea35459f5dd145da949
Zach Copland <zcopland16@gmail.com>
2016-03-13 19:56:36 -0400

    Background Fixes
    Content of the form is now logged and displayed on screen.

--- signupScript.js
+++ signupScript.js
@@ -1,28 +1,16 @@
-window.addEventListener("load", function() {
-	var submitBtn = document.getElementById("submitBtn");
-	submitBtn.addEventListener("click", function () {
-		//declaring variables
-		var firstName = document.getElementById("firstName").value;
-		var lastName = document.getElementById("lastName").value;
-		var emailAddress = document.getElementById("emailAddress").value;
-		console.log (firstName);
-		alert("Submitted");
+function signUp () {
+	//Declaring all variables
+	var firstName = document.getElementById("firstName").value;
+	var lastName = document.getElementById("lastName").value;
+	var phoneNumber = document.getElementById("phoneNumber").value;
+	var gender = document.getElementById("gender").value;
+	var sport = document.getElementById("sport").value;
+	var position = document.getElementById("position");
+	var form = document.getElementById("sportsForm");
 
-		//checking to see if form is filled out
-		//if (firstName.length > 2 && lastName.length > 3 && emailAddress.length >= 7) {
-			//now checking to see if email address is valid
-			/*function validateEmail(emailAddress) {
-	    		var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	    		return re.test(emailAddress);
-			}
-			if (re.test) {
-				alert(true);
-			} */ /*
-			alert("Your form has been submitted. Thank you.");
-		} 
-		//form has not been filled out
-		else {
-			alert("Please fill out the required fields.");
-		}
-	}) */
-});
+	//displaying content in the fields
+	alert(firstName + " " + lastName + " is a " + gender + " and wants to participate in " + sport + ". They can be reached at " + phoneNumber);
+
+	//clearing the form
+	form.reset();
+}
