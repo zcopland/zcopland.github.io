b7064bbaa005b5976863ca0c2888423d15f3269e
Zach <zcopland16@gmail.com>
2016-03-15 10:01:35 -0400

    Trying to implement google sheets API

--- signupScript.js
+++ signupScript.js
@@ -5,7 +5,7 @@ function signUp () {
 	var phoneNumber = document.getElementById("phoneNumber").value;
 	var gender = document.getElementById("gender").value;
 	var sport = document.getElementById("sport").value;
-	var position = document.getElementById("position");
+	var position = document.getElementById("position").value;
 	var form = document.getElementById("sportsForm");
 
 	//displaying content in the fields
@@ -13,4 +13,50 @@ function signUp () {
 
 	//clearing the form
 	form.reset();
+
+	
+
+	var baseurl = "https://script.google.com/macros/s/AKfycbxkE-n0t_3ynN1AIYx7wHUr4fjRu2m2tSrjEwR9s3d35jdCwfY/exec";
+	var hi = document.createElement("iframe");
+	hi.style.display = "hidden";
+	document.body.appendChild(hi);
+	console.log(hi);
+
+
+	var iframeError;
+
+	function submit() {   
+	    
+	    url = baseurl + "First="+firstName+"&Last="+lastName+"&Number="+phoneNumber+"&Sport="+sport+"&Position="+position;
+	    console.log(url);
+	    hi.src = url;
+	    iframeError = setTimeout("error()", 5000);  
+	}
+
+	function load(e) {
+	    console.log('success');
+	    //msg.innerHTML = "Your score has been registered.";
+	   
+	}
+
+	function error() {
+	    console.log('error');
+	    //msg.innerHTML = "There has been an error.";
+	}
+
+
+	hi.onload = function (e) {
+	        load(e);
+	        clearTimeout(iframeError);
+	    };
+
+
+
+	    submit();
+	// END OF DATA SEND CODE
+
+
+
+
+
 }
