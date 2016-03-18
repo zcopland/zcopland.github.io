5a9a2d502b765c91ffb1ca6e6719f9dcd226d614
Zach Copland <zcopland16@gmail.com>
2016-03-17 22:50:36 -0400

    Small Changes

--- signupScript.js
+++ signupScript.js
@@ -5,14 +5,51 @@ function signUp () {
 	var phoneNumber = document.getElementById("phoneNumber").value;
 	var gender = document.getElementById("gender").value;
 	var sport = document.getElementById("sport").value;
-	var position = document.getElementById("position");
+	var position = document.getElementById("position").value;
 	var form = document.getElementById("sportsForm");
+
+
 
 	//displaying content in the fields
 	alert(firstName + " " + lastName + " is a " + gender + " and wants to participate in " + sport + ". They can be reached at " + phoneNumber);
 
 	//clearing the form
 	form.reset();
+
+	var baseurl = "https://script.google.com/macros/s/AKfycbyOyRnqIM0q214Z2IfnY0_2EL_ByKVhId7UKBkjPgWtWt6HwRI/exec?";
+	var hi = document.createElement("iframe");
+	hi.style.display = "hidden";
+	document.body.appendChild(hi);
+	console.log(hi);
+
+
+	var iframeError;
+
+	function submit() {   
+	    
+	    url = baseurl + "First="+firstName+"&Last="+lastName+"&Number="+phoneNumber+"&Sex="+gender+"&Sport="+sport+"&Position="+position;
+	    console.log(url);
+	    hi.src = url;
+	    iframeError = setTimeout("error()", 5000);  
+	}
+
+	function load(e) {
+	    console.log('success');
+	    //msg.innerHTML = "Your score has been registered.";
+	   
+	}
+
+	function error() {
+	    console.log('error');
+	    //msg.innerHTML = "There has been an error.";
+	}
+
+	hi.onload = function (e) {
+	        load(e);
+	        clearTimeout(iframeError);
+	    };
+
+	    submit();
 }
 
 var sBackBtn = document.getElementById("sBackBtn");
