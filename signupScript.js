177cc19f364fb5abf002192b99215698701f8e3c
Zach <zcopland16@gmail.com>
2016-03-21 08:45:31 -0400

    Small changes

--- signupScript.js
+++ signupScript.js
@@ -18,14 +18,14 @@ function signUp () {
 	var form = document.getElementById("sportsForm");
 
 	//displaying content in the fields
-	alert(firstName + " " + lastName + " is a " + gender + " and wants to participate in " + sport + ". They can be reached at " + phoneNumber);
+	console.log(firstName + " " + lastName + " is a " + gender + " and wants to participate in " + sport + ". They can be reached at " + phoneNumber);
 
 	//clearing the form
 	form.reset();
 
 	var baseurl = "https://script.google.com/macros/s/AKfycbyOyRnqIM0q214Z2IfnY0_2EL_ByKVhId7UKBkjPgWtWt6HwRI/exec?";
 	var hi = document.createElement("iframe");
-	hi.style.display = "hidden";
+	hi.style.display = "none";
 	document.body.appendChild(hi);
 	console.log(hi);
 
@@ -42,13 +42,13 @@ function signUp () {
 
 	function load(e) {
 	    console.log('success');
-	    //msg.innerHTML = "Your score has been registered.";
+	    alert("Thank you. Your form has been submitted.")
 	   
 	}
 
 	function error() {
 	    console.log('error');
-	    //msg.innerHTML = "There has been an error.";
+	    alert("Error. Please try again later.")
 	}
 
 	hi.onload = function (e) {
