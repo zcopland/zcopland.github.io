ca26e30c0ddb96c54c35cfe014bb0e716a84b820
Zach <zcopland16@gmail.com>
2016-03-29 08:46:27 -0400

    Fixed it (You're Welcome)

--- Calendar/myCalendar.js
+++ Calendar/myCalendar.js
@@ -1,4 +1,4 @@
-var backBtn = document.getElementById("backBtn");
+var backBtn = document.getElementById("MCbackBtn");
   backBtn.addEventListener("click", function () {
     location.href="calendar.html";
     console.log("click");
