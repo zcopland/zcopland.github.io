1a0d7e3497fce6507d3c4270818c12a01f9e72ad
Zach <zcopland16@gmail.com>
2016-04-08 08:55:08 -0400

    Change 1

--- script.js
+++ script.js
@@ -1,68 +1,105 @@
-// window.addEventListener('load', function () {
-//   // At first, let's check if we have permission for notification
-//   // If not, let's ask for it
-//   if (window.Notification && Notification.permission !== "granted") {
-//     Notification.requestPermission(function (status) {
-//       if (Notification.permission !== status) {
-//         Notification.permission = status;
-//       }
-//     });
-//   }
+// Your Client ID can be retrieved from your project in the Google
+// Developer Console, https://console.developers.google.com
+var CLIENT_ID = '197201236939-teujohaodun956d4pahf89gbhkmss2su.apps.googleusercontent.com';
 
-//   var button = document.getElementById('button');
+var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 
-//   button.addEventListener('click', function () {
-//     // If the user agreed to get notified
-//     // Let's try to send ten notifications
-//     if (window.Notification && Notification.permission === "granted") {
-//       var i = 0;
-//       // Using an interval cause some browsers (including Firefox) are blocking notifications if there are too much in a certain time.
-//       var interval = window.setInterval(function () {
-//         // Thanks to the tag, we should only see the "Hi! 9" notification 
-//         var n = new Notification("Hi! " + i, {tag: 'soManyNotification'});
-//         if (i++ == 9) {
-//           window.clearInterval(interval);
-//         }
-//       }, 200);
-//     }
+var currentDay;
+/**
+ * Check if current user has authorized this application.
+ */
+function checkAuth() {
+  gapi.auth.authorize(
+    {
+      'client_id': CLIENT_ID,
+      'scope': SCOPES.join(' '),
+      'immediate': true
+    }, handleAuthResult);
+}
 
-//     // If the user hasn't told if he wants to be notified or not
-//     // Note: because of Chrome, we are not sure the permission property
-//     // is set, therefore it's unsafe to check for the "default" value.
-//     else if (window.Notification && Notification.permission !== "denied") {
-//       Notification.requestPermission(function (status) {
-//         if (Notification.permission !== status) {
-//           Notification.permission = status;
-//         }
+/**
+ * Handle response from authorization server.
+ *
+ * @param {Object} authResult Authorization result.
+ */
+function handleAuthResult(authResult) {
+  var authorizeDiv = document.getElementById('authorize-div');
+  if (authResult && !authResult.error) {
+    // Hide auth UI, then load client library.
+    authorizeDiv.style.display = 'none';
+    loadCalendarApi();
+  } else {
+    // Show auth UI, allowing the user to initiate authorization by
+    // clicking authorize button.
+    authorizeDiv.style.display = 'inline';
+  }
+}
 
-//         // If the user said okay
-//         if (status === "granted") {
-//           var i = 0;
-//           // Using an interval cause some browsers (including Firefox) are blocking notifications if there are too much in a certain time.
-//           var interval = window.setInterval(function () {
-//             // Thanks to the tag, we should only see the "Hi! 9" notification 
-//             var n = new Notification("Hi! " + i, {tag: 'soManyNotification'});
-//             if (i++ == 9) {
-//               window.clearInterval(interval);
-//             }
-//           }, 200);
-//         }
+/**
+ * Initiate auth flow in response to user clicking authorize button.
+ *
+ * @param {Event} event Button click event.
+ */
+function handleAuthClick(event) {
+  gapi.auth.authorize(
+    {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+    handleAuthResult);
+  return false;
+}
 
-//         // Otherwise, we can fallback to a regular modal alert
-//         else {
-//           alert("Alert!");
-//         }
-//       });
-//     }
+/**
+ * Load Google Calendar client library. List upcoming events
+ * once client library is loaded.
+ */
+function loadCalendarApi() {
+  gapi.client.load('calendar', 'v3', listUpcomingEvents);
+}
 
-//     // If the user refuses to get notified
-//     else {
-//       // We can fallback to a regular modal alert
-//       alert("Hi!");
-//     }
-//   });
-  
-// });
+/**
+ * Print the summary and start datetime/date of the next ten events in
+ * the authorized user's calendar. If no events are found an
+ * appropriate message is printed.
+ */
+function listUpcomingEvents() {
+  var request = gapi.client.calendar.events.list({
+    'calendarId': 'auburnschl.edu_n2mkfqv6kae2ovovra48abmk1o@group.calendar.google.com',
+    'timeMin': (new Date()).toISOString(),
+    'showDeleted': false,
+    'singleEvents': true,
+    'maxResults': 10,
+    'orderBy': 'startTime'
+  });
 
+  request.execute(function(resp) {
+    var events = resp.items;
+    appendPre('Upcoming events:');
 
+    if (events.length > 0) {
+      for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        var when = event.start.dateTime;
+        if (!when) {
+          when = event.start.date;
+        }
+        appendPre(event.summary + ' (' + when + ')')
+      }
+    } else {
+      appendPre('No upcoming events found.');
+    }
 
+  });
+}
+
+/**
+ * Append a pre element to the body containing the given message
+ * as its text node.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+  var pre = document.getElementById('output');
+  var textContent = document.createTextNode(message + '\n');
+  pre.appendChild(textContent);
+  console.log(textContent);
+}
+//console.log(currentDay);
