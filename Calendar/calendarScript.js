8f2c751df079da008c65f26c60ea93cd65a0096a
Zach <zcopland16@gmail.com>
2016-04-14 09:09:10 -0400

    Added functions

---
+++ Calendar/calendarScript.js
@@ -0,0 +1,75 @@
+function varsityFootball () {
+	console.log("Football clicked.");
+
+}
+
+function crossCountry () {
+	console.log("Cross Country clicked.");
+
+}
+
+function soccer () {
+	console.log("Soccer clicked.");
+
+}
+
+function fieldHockey () {
+	console.log("Field Hockey clicked.");
+
+}
+
+function golf () {
+	console.log("Golf clicked.");
+
+}
+
+function nordicSki () {
+	console.log("Nordic Ski clicked.");
+
+}
+
+function basketball () {
+	console.log("Basketball clicked.");
+
+}
+
+function indoorTrack () {
+	console.log("Indoor Track clicked.");
+
+}
+
+function cheering () {
+	console.log("Cheering clicked.");
+
+}
+
+function alpineSki () {
+	console.log("Alpine Ski clicked.");
+
+}
+
+function hockey () {
+	console.log("Hockey clicked.");
+
+}
+
+function swimming () {
+	console.log("Swimming clicked.");
+
+}
+
+function outdoorTrack () {
+	console.log("Outdoor Track clicked.");
+
+}
+
+function baseball () {
+	console.log("Baseball clicked.");
+
+}
+
+function studentActivities () {
+	console.log("Student Activities clicked.");
+
+}
+
